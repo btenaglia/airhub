b6aa2eed478b05dbc836b1830a69074bb990682c
Bruno M. Tenaglia <brunomartintenaglia@gmail.com>
2019-07-10 10:50:02 -0300

    Cambio paypal credentials to sandbox

--- laravel/app/services/PaymentService.php
+++ laravel/app/services/PaymentService.php
@@ -23,12 +23,12 @@ use URL;
 class PaymentService extends BaseService
 {
     //SANDBOX
-    //private static $clientId = "AZo7YRikhHlZX4Oh3ry0b-gCEMVM1Ig1jWkN9XStO5fYewPLScQB64XZtbpVoFUFTrCA3rhWWbqBBLe9";
-    //private static $paypalSecret = "EJyGx7VNb2nMqTKlruX5aRRV-U2vyIcGkx17mT6bq-RudqtZmNRXdUsxXrjc_6EWD-mBzPSvtpu4MDPn";
-
-    //PRODUCTION
     private static $clientId = "AViTmsSgc7pfq0rbQxKsBu10Jad9ozAXBZsrz0Fx26QYiiEB9TnGw-gAiYIxyQPfDXUcnn_aOSq3DGqT";
     private static $paypalSecret = "EDuLdJ_wKjKTZafOows_skhvlUbn0W2iAdBdRkCFt0fqqh-MBBn-kcJ9ojLkEhGaCCufQZdlql7yHYAg";
+
+    //PRODUCTION
+    // private static $clientId = "AViTmsSgc7pfq0rbQxKsBu10Jad9ozAXBZsrz0Fx26QYiiEB9TnGw-gAiYIxyQPfDXUcnn_aOSq3DGqT";
+    // private static $paypalSecret = "EDuLdJ_wKjKTZafOows_skhvlUbn0W2iAdBdRkCFt0fqqh-MBBn-kcJ9ojLkEhGaCCufQZdlql7yHYAg";
 
     const FIRST_ITEM = 0;
 
@@ -44,8 +44,8 @@ class PaymentService extends BaseService
             )
         );
         $this->apiContext->setConfig([
-            "mode" => "live",
-            //"mode" => "sandbox",
+            //"mode" => "live",
+            "mode" => "sandbox",
         ]);
 
     }
